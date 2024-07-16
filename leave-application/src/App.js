d4db324991caa67a4448134bad48787b3bc6cf0e
cipher <fynhacker@gmail.com>
2024-07-16 15:50:34 +0530

    Initial commit

---
+++ leave-application/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import LeaveForm from "./components/LeaveForm";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <LeaveForm />
+    </div>
+  );
+}
+
+export default App;
